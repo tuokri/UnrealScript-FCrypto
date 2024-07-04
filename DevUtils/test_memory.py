3c2d8ad53dfe05b00251a8661b8e2876fd0cc0c3
Tuomo Kriikkula <tuokri@tuta.io>
2024-07-04 13:21:19 +0300

    Update copyright years

--- DevUtils/test_memory.py
+++ DevUtils/test_memory.py
@@ -1,6 +1,6 @@
 # MIT License
 #
-# Copyright (c) 2023 Tuomo Kriikkula
+# Copyright (c) 2023-2024 Tuomo Kriikkula
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
