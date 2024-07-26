49b90bafeb505d76883d075ab77390cf42664569
Tuomo Kriikkula <tuokri@tuta.io>
2024-07-26 14:12:13 +0300

    Update VcRedist to work with unsupported redistributables

--- UDKTests/install_vcredist.ps1
+++ UDKTests/install_vcredist.ps1
@@ -4,7 +4,7 @@ New-Item "C:\Temp\" -ItemType Directory -ErrorAction SilentlyContinue
 
 Install-Module -Name VcRedist -Force
 New-Item -Path C:\Temp\VcRedist -ItemType Directory -Force
-$VcList = Get-VcList -Release "2012"
+$VcList = Get-VcList -Export Unsupported | Where-Object { $_.Release -eq "2012" }
 Save-VcRedist -VcList $VcList -Path C:\Temp\VcRedist
 Install-VcRedist -Path C:\Temp\VcRedist -VcList $VcList -Silent
 
