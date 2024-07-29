a854eb7c1411f081d7fc10615f3600de80e5aa3c
Tuomo Kriikkula <tuokri@tuta.io>
2024-07-29 21:47:37 +0300

    Fix Install-VcRedist warning

--- UDKTests/install_vcredist.ps1
+++ UDKTests/install_vcredist.ps1
@@ -5,7 +5,7 @@ New-Item "C:\Temp\" -ItemType Directory -ErrorAction SilentlyContinue
 Install-Module -Name VcRedist -Force
 New-Item -Path C:\Temp\VcRedist -ItemType Directory -Force
 $VcList = Get-VcList -Export Unsupported | Where-Object { $_.Release -eq "2012" }
-Save-VcRedist -VcList $VcList -Path C:\Temp\VcRedist
-Install-VcRedist -Path C:\Temp\VcRedist -VcList $VcList -Silent
+$VcList = Save-VcRedist -VcList $VcList -Path C:\Temp\VcRedist
+Install-VcRedist -VcList $VcList -Silent
 
 Exit $LASTEXITCODE
